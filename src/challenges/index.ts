047e17702ef87f0fe4a3850f6d8e5af5a549aa04
Dani Lupión <danilupion@gmail.com>
2020-12-17 15:08:17 +0100

    Challenge 5

--- src/challenges/index.ts
+++ src/challenges/index.ts
@@ -2,5 +2,7 @@ import challenge1 from '../challenges/1.json';
 import challenge2 from '../challenges/2.json';
 import challenge3 from '../challenges/3.json';
 import challenge4 from '../challenges/4.json';
+import challenge5 from '../challenges/5.json';
 
-export default [challenge1, challenge2, challenge3, challenge4];
+
+export default [challenge1, challenge2, challenge3, challenge4, challenge5];
