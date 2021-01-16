1abdb4e96b7998d2fa6dd0841d81d1fe7506880e
Dani Lupión <danilupion@gmail.com>
2021-01-16 19:16:36 +0100

    Challenge 6

--- src/challenges/index.ts
+++ src/challenges/index.ts
@@ -3,6 +3,7 @@ import challenge2 from '../challenges/2.json';
 import challenge3 from '../challenges/3.json';
 import challenge4 from '../challenges/4.json';
 import challenge5 from '../challenges/5.json';
+import challenge6 from '../challenges/6.json';
 
 
-export default [challenge1, challenge2, challenge3, challenge4, challenge5];
+export default [challenge1, challenge2, challenge3, challenge4, challenge5, challenge6];
