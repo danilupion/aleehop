e664c57d48e7409f57e69be47ca778c20878221d
Dani Lupión <danilupion@gmail.com>
2022-11-17 21:08:36 +0100

    Challenge 5 and more phrases in challenge 4

--- src/challenges/index.ts
+++ src/challenges/index.ts
@@ -4,8 +4,9 @@ import challenge1 from '../challenges/1.json';
 import challenge2 from '../challenges/2.json';
 import challenge3 from '../challenges/3.json';
 import challenge4 from '../challenges/4.json';
+import challenge5 from '../challenges/5.json';
 
-const challenges = [challenge1, challenge2, challenge3, challenge4];
+const challenges = [challenge1, challenge2, challenge3, challenge4, challenge5];
 
 export enum Type {
   Syllable = 'syllable',
