c3caf35b31e31a6ef1b3043b8d7326b774f97d94
Dani Lupión <danilupion@gmail.com>
2023-01-20 09:52:40 +0100

    Challenge 11

--- src/challenges/index.ts
+++ src/challenges/index.ts
@@ -2,6 +2,7 @@ import { shuffle } from 'lodash';
 
 import challenge1 from '../challenges/1.json';
 import challenge10 from '../challenges/10.json';
+import challenge11 from '../challenges/11.json';
 import challenge2 from '../challenges/2.json';
 import challenge3 from '../challenges/3.json';
 import challenge4 from '../challenges/4.json';
@@ -22,6 +23,7 @@ const challenges = [
   challenge8,
   challenge9,
   challenge10,
+  challenge11,
 ];
 
 export enum Type {
@@ -35,20 +37,24 @@ export default challenges;
 export const getItems = (index: number, type: Type, backwards: boolean, take?: number) => {
   const chosenChallenges = challenges.slice(backwards ? 0 : index - 1, index);
 
-  const items = chosenChallenges
-    .map((c) => {
-      switch (type) {
-        case Type.Syllable:
-          return c.syllables;
-        case Type.Word:
-          return c.words;
-        case Type.Phrase:
-          return c.phrases;
-        default:
-          return [];
-      }
-    })
-    .flat();
+  const items = [
+    ...new Set(
+      chosenChallenges
+        .map((c) => {
+          switch (type) {
+            case Type.Syllable:
+              return c.syllables;
+            case Type.Word:
+              return c.words;
+            case Type.Phrase:
+              return c.phrases;
+            default:
+              return [];
+          }
+        })
+        .flat(),
+    ),
+  ];
 
   if (take) {
     return shuffle(items).slice(0, take);
